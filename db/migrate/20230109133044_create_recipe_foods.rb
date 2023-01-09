2d2fa761af7ca14de27f6820e246cffed00168a5
shyusu4 <shyusupova4@gmail.com>
2023-01-09 18:50:41 +0500

    Update migrations and migrate into schema

--- db/migrate/20230109133044_create_recipe_foods.rb
+++ db/migrate/20230109133044_create_recipe_foods.rb
@@ -1,6 +1,9 @@
 class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :food, null: false, foreign_key: true
+      t.string :quantity
 
       t.timestamps
     end
