2b3c2bb942aa0125f911c5e68afd86456a7eb472
shyusu4 <shyusupova4@gmail.com>
2023-01-10 23:12:37 +0500

    Seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "users#index", as: "users"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
+  
+  resources :users, only: [:index, :show] do
+    resources :foods, only: [:index, :show, :new, :create, :destroy]
+    resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+      resources :recipe_foods, only: [:new, :edit, :create, :update, :destroy]
+      resources :general_shopping_list, only: [:index]
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_10_130707) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
