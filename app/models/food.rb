03db3e5d250aa896964a6335cc8d8d9650a39d34
shyusu4 <shyusupova4@gmail.com>
2023-01-11 15:27:44 +0500

    Fix linter errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,7 @@
 class Food < ApplicationRecord
-    belongs_to :user
-    validates :name, presence: true
-    validates :measurement_unit, presence: true
-    validates :price, presence: true, numericality: { greater_than_or_equal: 0 }
-    validates :quantity, presence: true, numericality: { greater_than_or_equal: 0 }    
+  belongs_to :user
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal: 0 }
+  validates :quantity, presence: true, numericality: { greater_than_or_equal: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-         has_many :foods, dependent: :destroy
-         has_many :recipes, dependent: :destroy
-         validates :name, presence: true
-         
-         ROLES = %i[admin default].freeze
-       
-         def is?(requested_role)
-           role == requested_role.to_s
-         end
+  has_many :foods, dependent: :destroy
+  has_many :recipes, dependent: :destroy
+  validates :name, presence: true
+
+  ROLES = %i[admin default].freeze
+
+  def is?(requested_role)
+    role == requested_role.to_s
+  end
 end
