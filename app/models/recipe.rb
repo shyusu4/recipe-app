05bdaa147a520831e679d94cffbfaed83092614f
shyusu4 <shyusupova4@gmail.com>
2023-01-11 21:25:35 +0500

    Add validation in recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+    belongs_to :user, class_name: 'User'
+  validates :name, presence: true
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :description, presence: true, length: { minimum: 15 }
 end
