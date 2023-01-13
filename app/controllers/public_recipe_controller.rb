befedc7ab8e9b36e3781f8659446199e9970a2c6
shyusu4 <shyusupova4@gmail.com>
2023-01-13 16:29:37 +0500

    Add public recipes page

--- app/controllers/public_recipe_controller.rb
+++
@@ -1,6 +0,0 @@
-class PublicRecipesController < ApplicationController
-  def index
-    @recipes = Recipe.all.where(public: true).includes([:user])
-    @ingredients = RecipeFood.all.includes([:food])
-  end
-end
