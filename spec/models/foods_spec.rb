89f6d634f55c42de51608960f98f03956c61ea31
shyusu4 <shyusupova4@gmail.com>
2023-01-13 19:00:37 +0500

    Add foods model spec

---
+++ spec/models/foods_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Food, type: :model do
+    before :each do
+        @food = Food.new(user: @user, name: 'Apple', measurement_unit: 'grams', price: 1.59, quantity: 4)
+      end
+
+  it 'should be named apple' do
+    expect(@food.name).to eql('Apple')
+  end
+
+  it 'should have price of 2' do
+    expect(@food.price).to eql(1.59)
+  end
+
+  it 'should have measurement_unit grams' do
+    expect(@food.measurement_unit).to eql('grams')
+  end
+end
