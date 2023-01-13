d8169f1b9605c4e226b19ab750f1d8462e67a703
shyusu4 <shyusupova4@gmail.com>
2023-01-13 19:18:44 +0500

    Add recipe_food model spec

---
+++ spec/models/recipe_food.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe RecipeFood, type: :model do
+  before :each do
+    @user = User.create(name: 'John Doe')
+    @recipe = Recipe.create(user: @user, name: 'Sushi', preparation_time: '1 hour', cooking_time: '20 minutes',
+                            description: 'Lorem ipsum', public: false)
+    @food = Food.create(user: @user, name: 'Salmon', measurement_unit: 'grams', price: 17.55, quantity: 3)
+    @recipe_food = RecipeFood.new(recipe: @recipe, food: @food, quantity: 2)
+  end
+
+    it 'should have valid attributes' do
+      expect(@recipe_food).to be_valid
+    end
+
+    it 'should have quantity of 2' do
+      expect(@recipe_food.quantity) eql(2)
+    end
+
+  context 'Testing Associations' do
+    it 'belongs_to recipe' do
+      assoc = RecipeFood.reflect_on_association(:recipe)
+      expect(assoc.macro).to eq :belongs_to
+    end
+
+    it 'belongs_to food' do
+      assoc = RecipeFood.reflect_on_association(:food)
+      expect(assoc.macro).to eq :belongs_to
+    end
+  end
+end
